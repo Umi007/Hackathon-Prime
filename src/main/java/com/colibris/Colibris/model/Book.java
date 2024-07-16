6fa102a15e2ef8ab9d9088b0a8cf9a1a00d5b947
Arrowsmith <sarrowsmith@uk-h7xyl656w3.uk.deloitte.com>
2024-07-16 13:41:42 +0100

    adds basic models

---
+++ src/main/java/com/colibris/Colibris/model/Book.java
@@ -0,0 +1,39 @@
+package com.colibris.Colibris.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+// Specifies that this class is an entity and will be mapped to a database table
+@Table(name = "BOOKS")
+public class Book {
+    // Specifies the primary key of the entity
+    @Id
+    // Specifies how the primary key should be generated
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    private String genre;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    // Default constructor for the Condition class.
+    public Book(int id, String title, String author, User owner)
+    {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.owner = owner;
+    }
+}
---
+++ src/main/java/com/colibris/Colibris/model/User.java
@@ -0,0 +1,24 @@
+package com.colibris.Colibris.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+// Specifies that this class is an entity and will be mapped to a database table
+@Table(name = "USERS")
+public class User {
+    // Specifies the primary key of the entity
+    @Id
+    // Specifies how the primary key should be generated
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
