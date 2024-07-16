1c028b7726bcc0ae3ac71ebf730f1ba50203d3bb
Arrowsmith <sarrowsmith@uk-h7xyl656w3.uk.deloitte.com>
2024-07-16 15:28:43 +0100

    add basic controllers

---
+++ src/main/java/com/colibris/Colibris/controller/BookController.java
@@ -0,0 +1,19 @@
+package com.colibris.Colibris.controller;
+
+import com.colibris.Colibris.repository.BookRepository;
+import com.colibris.Colibris.repository.LoanRepository;
+import com.colibris.Colibris.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    LoanRepository loanRepository;
+}
