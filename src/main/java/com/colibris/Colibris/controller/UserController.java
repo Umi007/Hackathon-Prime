cc7413266596634236af83659abc38f178c7f9db
wimdavies <william.johnston.davies@gmail.com>
2024-07-17 16:16:27 +0100

    Change RequestBody params for UserController#create

--- src/main/java/com/colibris/Colibris/controller/UserController.java
+++ src/main/java/com/colibris/Colibris/controller/UserController.java
@@ -6,8 +6,11 @@ import com.colibris.Colibris.repository.LoanRepository;
 import com.colibris.Colibris.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -15,7 +18,10 @@ public class UserController {
     UserRepository userRepository;
 
     @PostMapping("/api/users")
-    public User addUser(@RequestParam String username, @RequestParam String email, @RequestParam String password) {
+    public User addUser(@RequestBody Map<String, Object> requestBody) {
+        String username = requestBody.get("username").toString();
+        String email = requestBody.get("email").toString();
+        String password = requestBody.get("password").toString();
         User user = new User(username, email, password);
         return userRepository.save(user);
     }
