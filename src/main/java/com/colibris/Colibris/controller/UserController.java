144e6b5a455663c6d0039f8b7e47f87c1365dc8d
wimdavies <william.johnston.davies@gmail.com>
2024-07-17 12:41:02 +0100

    Add UserController#create

--- src/main/java/com/colibris/Colibris/controller/UserController.java
+++ src/main/java/com/colibris/Colibris/controller/UserController.java
@@ -1,19 +1,22 @@
 package com.colibris.Colibris.controller;
 
+import com.colibris.Colibris.model.User;
 import com.colibris.Colibris.repository.BookRepository;
 import com.colibris.Colibris.repository.LoanRepository;
 import com.colibris.Colibris.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     @Autowired
-    BookRepository bookRepository;
-
-    @Autowired
     UserRepository userRepository;
 
-    @Autowired
-    LoanRepository loanRepository;
+    @PostMapping("/api/users")
+    public User addUser(@RequestParam String username, @RequestParam String email, @RequestParam String password) {
+        User user = new User(username, email, password);
+        return userRepository.save(user);
+    }
 }
